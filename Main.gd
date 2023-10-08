42b2d078e39aa839063040a5630a208603f5037d
Steven Tran <StevenDoTran@gmail.com>
2023-10-08 00:45:58 -0500

    V1.0: Completed Godot 2D Tutorial

---
+++ Main.gd
@@ -0,0 +1,64 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	new_game()
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+	
+func new_game():
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
+
+func _on_score_timer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
+func _on_mob_timer_timeout():
+	# create a new instance of the mob scene
+	var mob = mob_scene.instantiate()
+	
+	# choose a random location on Path2D (MobSpawnLocation)
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	
+	# set the mob's direction perpendicular to the path direction
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	#set the mob's position to a random location
+	mob.position = mob_spawn_location.position
+	
+	# add some randomness to the direction
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	# choose the velocity for the mob
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	# spawn the mob by adding it to the main scene
+	add_child(mob)
---
+++ Scenes/Player.gd
@@ -0,0 +1,70 @@
+extends Area2D
+
+# How fast the player will move (pixels/sec).
+# The export keyword allows us to modify the variable in the Inspector panel
+@export var speed = 400 
+# Size of the game window
+var screen_size
+signal hit
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# The player's movement vector
+	# Initialize as zero for if play didn't give input during a frame to prevent movement
+	var velocity = Vector2.ZERO
+	
+	# Check for if input was given and perform action
+	# Max of (1, 1) for if up and right and min of (-1, -1) for if down and left
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x += -1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y += -1
+		
+	# If both x and y have a absolute value of 1, then the player would move faster diagonally
+	# then if it was horizontal. To prevent this, we normalize the vector, which sets the length to 1,
+	# then multiply by the speed
+	# Also checks if the player is moving to choose to play animation
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		# $ is shorthand for get_node()
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	# Use clamp() to restrict the range of the node in a certain range
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	# if moving left or right
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# if velocity.x == -1, then it is moving left, flip horizontally
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	# if moving up or down
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		# if velocity.y == 1, then it is moving down, flip vertically
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit
+	hit.emit() # Emit the custom hit signal
+	# Must be deferred as we can't change physics properties on a physics callback
+	# Disable the player's collision so we don't trigger the hit signal more than once
+	# set_deferred() tells Godot to wait to disable the shape until it's safe to do so
+	$CollisionShape2D.set_deferred("disabled", true)
